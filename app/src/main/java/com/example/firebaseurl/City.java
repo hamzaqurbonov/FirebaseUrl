9dba628ece577bd9bc95c869a0f9ae2a19bc1964
hamzaqurbonov <77965943+hamzaqurbonov@users.noreply.github.com>
2024-03-28 17:30:24 +0500

    O'zgarishsiz
    O'zgarishsiz

---
+++ app/src/main/java/com/example/firebaseurl/City.java
@@ -0,0 +1,55 @@
+package com.example.firebaseurl;
+
+import android.util.Log;
+
+import java.util.List;
+
+public class City {
+    private String name;
+    private String id;
+    private String idu;
+    private String state;
+    private String country;
+    private boolean capital;
+    private long population;
+    private List<String> regions;
+
+    public City() {}
+
+    public City (String name,String state, String country, boolean capital, long population, List<String> regions) {
+        Log.d("demo1", name + state);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public String getIdu() {
+//        return idu;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public boolean isCapital() {
+        return capital;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public List<String> getRegions() {
+        return regions;
+    }
+}
--- app/src/main/java/com/example/firebaseurl/MainActivity.java
+++ app/src/main/java/com/example/firebaseurl/MainActivity.java
@@ -13,8 +13,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
@@ -28,6 +30,7 @@ import com.google.firebase.firestore.Source;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,7 +38,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    City city1 = new City();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference cities = db.collection("cities");
 
@@ -83,16 +86,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        // ------------------------------------------------------------
+        // ------------------------Modul dan ma'lumot olish------------------------------------
 
 //        DocumentReference docRef2 = db.collection("cities").document("BJ");
 //        docRef2.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
 //            @Override
 //            public void onSuccess(DocumentSnapshot documentSnapshot) {
-////                City city = documentSnapshot.toObject(City.class);
+//                City city = documentSnapshot.toObject(City.class);
+//                Log.d("demo1", String.valueOf(city.getCountry()));
 //            }
 //        });
-
 
 
         // -------------------------Hamma obekt ichida aylanadi-----Faqt true lar------------------------------
@@ -229,77 +232,175 @@ public class MainActivity extends AppCompatActivity {
 //                });
         //
 
+        // -------------------Жамисини capital-true ларини тортиб берди----------------------------------------
+
+//        db.collection("cities")
+//                .whereEqualTo("capital", true)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d("demo1", document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d("demo1", "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
         // ------------------------------------------------------------
 
-        db.collection("cities")
-                .whereEqualTo("capital", true)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("demo1", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d("demo1", "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+//        db.collection("cities")
+//                .whereEqualTo("capital", true)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d("demo1", document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d("demo1", "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+        // --------------------objectExample", nestedData  ёзиш ----------------------------------------
+
+//        Map<String, Object> docData = new HashMap<>();
+//        docData.put("stringExample", "Hello world!");
+//        docData.put("booleanExample", true);
+//        docData.put("numberExample", 3.14159265);
+//        docData.put("dateExample", new Timestamp(new Date()));
+//        docData.put("listExample", Arrays.asList(1, 2, 3));
+//        docData.put("nullExample", null);
+//
+//        Map<String, Object> nestedData = new HashMap<>();
+//        nestedData.put("a", 5);
+//        nestedData.put("b", true);
+//
+//        docData.put("objectExample", nestedData);
+//
+//        db.collection("data").document("one")
+//                .set(docData)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//                        Log.d("demo1", "DocumentSnapshot successfully written!");
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w("demo1", "Error writing document", e);
+//                    }
+//                });
+
+
+        // ---------------------------Modulga yuborish--Yozish-------------------------------
+//        City city = new City("Los Angeles", "CA", "USA",
+//                false, 5000000L, Arrays.asList("west_coast", "sorcal"));
+//        db.collection("citie").document("LA").set(city);
+
+
+
+        // -------------------Stringni kalitga aylantirish---add bilan-yozish-------------------------------------
+//        db.collection("cities").document("new-city-id").set(data);
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("name", "Tokyo");
+//        data.put("country", "Japan");
+//
+//        db.collection("citie")
+//                .add(data)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+
+        // -------------------------ichki obekt yasash--kerekli---------------------------------
+
+//        db.collection("users").document("frank")
+//                .update(
+//                        "age", 13,
+//                        "favorites.color", "Red",
+//                        "favorites.name", "Blue",
+//                        "4.3.name", "Black",
+//                        "4.3.age", "36",
+//                        "4.4.age", "37"
+//                );
+        // ------------------------------------------------------------
+
+
+
+
         // ------------------------------------------------------------
         // ------------------------------------------------------------
         // ------------------------------------------------------------
         // ------------------------------------------------------------
         // ------------------------------------------------------------
         // ------------------------------------------------------------
+        // ------------------------------------------------------------
 
 
-        CollectionReference cities = db.collection("cities");
 
-        Map<String, Object> data1 = new HashMap<>();
-        data1.put("name", "San Francisco");
-        data1.put("state", "CA");
-        data1.put("country", "USA");
-        data1.put("capital", false);
-        data1.put("population", 860000);
-        data1.put("regions", Arrays.asList("west_coast", "norcal"));
-        cities.document("SF").set(data1);
 
-        Map<String, Object> data2 = new HashMap<>();
-        data2.put("name", "Los Angeles");
-        data2.put("state", "CA");
-        data2.put("country", "USA");
-        data2.put("capital", false);
-        data2.put("population", 3900000);
-        data2.put("regions", Arrays.asList("west_coast", "socal"));
-        cities.document("LA").set(data2);
 
-        Map<String, Object> data3 = new HashMap<>();
-        data3.put("name", "Washington D.C.");
-        data3.put("state", null);
-        data3.put("country", "USA");
-        data3.put("capital", true);
-        data3.put("population", 680000);
-        data3.put("regions", Arrays.asList("east_coast"));
-        cities.document("DC").set(data3);
-
-        Map<String, Object> data4 = new HashMap<>();
-        data4.put("name", "Tokyo");
-        data4.put("state", null);
-        data4.put("country", "Japan");
-        data4.put("capital", true);
-        data4.put("population", 9000000);
-        data4.put("regions", Arrays.asList("kanto", "honshu"));
-        cities.document("TOK").set(data4);
-
-        Map<String, Object> data5 = new HashMap<>();
-        data5.put("name", "Beijing");
-        data5.put("state", null);
-        data5.put("country", "China");
-        data5.put("capital", true);
-        data5.put("population", 21500000);
-        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
-        cities.document("BJ").set(data5);
+//
+//        CollectionReference cities = db.collection("cities");
+//
+//        Map<String, Object> data1 = new HashMap<>();
+//        data1.put("name", "San Francisco");
+//        data1.put("state", "CA");
+//        data1.put("country", "USA");
+//        data1.put("capital", false);
+//        data1.put("population", 860000);
+//        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+//        cities.document("SF").set(data1);
+//
+//        Map<String, Object> data2 = new HashMap<>();
+//        data2.put("name", "Los Angeles");
+//        data2.put("state", "CA");
+//        data2.put("country", "USA");
+//        data2.put("capital", false);
+//        data2.put("population", 3900000);
+//        data2.put("regions", Arrays.asList("west_coast", "socal"));
+//        cities.document("LA").set(data2);
+//
+//        Map<String, Object> data3 = new HashMap<>();
+//        data3.put("name", "Washington D.C.");
+//        data3.put("state", null);
+//        data3.put("country", "USA");
+//        data3.put("capital", true);
+//        data3.put("population", 680000);
+//        data3.put("regions", Arrays.asList("east_coast"));
+//        cities.document("DC").set(data3);
+//
+//        Map<String, Object> data4 = new HashMap<>();
+//        data4.put("name", "Tokyo");
+//        data4.put("state", null);
+//        data4.put("country", "Japan");
+//        data4.put("capital", true);
+//        data4.put("population", 9000000);
+//        data4.put("regions", Arrays.asList("kanto", "honshu"));
+//        cities.document("TOK").set(data4);
+//
+//        Map<String, Object> data5 = new HashMap<>();
+//        data5.put("name", "Beijing");
+//        data5.put("state", null);
+//        data5.put("country", "China");
+//        data5.put("capital", true);
+//        data5.put("population", 21500000);
+//        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+//        cities.document("BJ").set(data5);
 
 
 
